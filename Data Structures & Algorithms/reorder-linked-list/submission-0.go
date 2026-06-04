5a6abc6bb7b8d3e6d785173250c2cd7e02ec3fb6
Maharoof Rashi <connectmaharoof@gmail.com>
2026-06-05 00:15:33 +0400

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func reorderList(head *ListNode) {
+    nodes := []*ListNode{}
+	curr := head
+	for curr != nil {
+		nodes = append(nodes, curr)
+		curr = curr.Next
+	}
+
+	left, right := 0, len(nodes)-1
+	for left < right {
+		nodes[left].Next = nodes[right]
+		left++
+
+		if left == right {
+			break
+		}
+
+		nodes[right].Next = nodes[left]
+		right--
+	}
+
+	nodes[left].Next = nil
+}
