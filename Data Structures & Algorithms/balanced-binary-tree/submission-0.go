1f405c26f481f542ac00d12860e2e02f482ca24e
Maharoof Rashi <connectmaharoof@gmail.com>
2026-06-15 01:18:45 +0400

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.go
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isBalanced(root *TreeNode) bool {
+    return checkHeight(root) != -1
+}
+
+func checkHeight(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+
+	leftHeight := checkHeight(node.Left)
+	if leftHeight == -1 {
+		return -1
+	}
+
+	rightHeight := checkHeight(node.Right)
+	if rightHeight == -1 {
+		return -1
+	}
+
+	if abs(leftHeight - rightHeight) > 1 {
+		return -1
+	}
+
+	return max(leftHeight, rightHeight) + 1
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
