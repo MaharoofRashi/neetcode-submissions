a6ea71332b5a3c278a6c8565e8bf2d0af0f13772
Maharoof Rashi <connectmaharoof@gmail.com>
2026-06-21 02:30:30 +0400

    Add: count-good-nodes-in-binary-tree - submission-0

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-0.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func goodNodes(root *TreeNode) int {
+    var dfs func(node *TreeNode, maxSoFar int) int
+	dfs = func(node *TreeNode, maxSoFar int) int {
+		if node == nil {
+			return 0
+		}
+
+		count := 0
+		if node.Val >= maxSoFar {
+			count = 1
+			maxSoFar = node.Val
+		}
+
+		count += dfs(node.Left, maxSoFar)
+		count += dfs(node.Right, maxSoFar)
+
+		return count
+	}
+
+	return dfs(root, root.Val)
+}
