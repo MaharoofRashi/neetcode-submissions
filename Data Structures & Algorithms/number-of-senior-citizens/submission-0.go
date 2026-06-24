5c30d038b776d09e864021271162f52151ddad64
Maharoof Rashi <connectmaharoof@gmail.com>
2026-06-25 01:08:34 +0400

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.go
@@ -0,0 +1,14 @@
+func countSeniors(details []string) int {
+
+	count := 0
+
+	for _, v := range details {
+		age, _ := strconv.Atoi(string(v[11:13]))
+
+		if age > 60 {
+			count++
+		}
+	}
+
+	return count
+}
