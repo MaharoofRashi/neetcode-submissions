781aec7549db1bb6ffdb9ea3ba826670467f955c
Maharoof Rashi <connectmaharoof@gmail.com>
2026-07-30 00:23:22 +0400

    Add: monotonic-array - submission-0

---
+++ Data Structures & Algorithms/monotonic-array/submission-0.go
@@ -0,0 +1,13 @@
+func isMonotonic(nums []int) bool {
+    inc, dec := true, true
+	for i := 1; i < len(nums); i++ {
+		if nums[i] > nums[i-1] {
+			dec = false
+		}
+		if nums[i] < nums[i-1] {
+			inc = false
+		}
+	}
+
+	return inc || dec
+}
